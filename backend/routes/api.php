3d3eb363823055097ad518f45c1253cf28f4a02a
hieu1509 <hhieu1592004@gmail.com>
2024-09-24 00:58:15 +0700

    lan2

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\api\ApiauthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+
+
+use App\Http\Controllers\ApiProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();});
+Route::post('/postRegister', [ApiauthController::class, 'postRegister']);
