3ab3b15a052a82faa64d9b141b0198762c308201
thanhndph40327 <thanhthotdz@gmail.com>
2024-10-03 16:15:56 +0700

    lan2

--- backend/app/Http/Controllers/Api/ApiauthController.php
+++ backend/app/Http/Controllers/Api/ApiauthController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\forgotPasswordMail;
+use App\Models\password_reset_tokens;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class ApiauthController extends Controller
 {
@@ -44,18 +48,93 @@ class ApiauthController extends Controller
     {
         $user = User::findOrFail($id);
         $param = $request->except('_token', '_method');
-        // dd($param);
-       $check= $user->update($param);
-    if($check){
+        $check = $user->update($param);
+        if ($check) {
+            return response()->json([
+                'message' => 'Cập nhật tài khoản thành công',
+                'user' => $user,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Cập nhật tài khoản không thành công',
+                'user' => $user,
+            ], 404);
+        }
+    }
+
+    public function forgot_password()
+    {
+        /* trả về giao diện nhập email */
+    }
+
+    public function check_forgot_password(Request $request)
+    {
+        $email = $request->email;
+        $user = User::where('email', $email)->first();
+        if (!$user) {
+            return response()->json([
+                'message' => 'Email không tồn tại trong hệ thống'
+            ], 404);
+        }
+
+        $token = Str::random(40);
+        $tokendata = [
+            'email' => $email,
+            'token' => $token,
+        ];
+
+        if (password_reset_tokens::create($tokendata)) {
+            Mail::to($email)->send(new forgotPasswordMail($user, $token));
+            return response()->json([
+                'message' => 'Vui lòng kiểm tra email',
+                'token' => $token
+            ]);
+        } else {
+            return response()->json([
+                'message' => 'Chưa được gửi vào email'
+            ]);
+        }
+    }
+
+    public function reset_password($token)
+    {
+        $tokendata = password_reset_tokens::where('token', $token)->first();
+        if (!$tokendata) {
+            return response()->json([
+                'message' => 'Token không hợp lệ hoặc đã hết hạn'
+            ], 400);
+        }
+
         return response()->json([
-            'message' => 'Cập nhật tài khoản thành công',
-            'user' => $user,
-        ], 200);  
-    }else{
+            'message' => 'Đã trả về trang gì đó',
+            'token' => $tokendata
+        ]);
+    }
+
+    public function check_reset_password(Request $request, $token)
+    {
+
+        $tokendata = password_reset_tokens::where('token', $token)->firstOrFail();
+
+
+        $user = User::where('email', $tokendata->email)->first();
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'Người dùng không tồn tại'
+            ], 404);
+        }
+
+
+        $user->update([
+            'password' => Hash::make($request->password),
+        ]);
+
+
+        password_reset_tokens::where('email', $tokendata->email)->delete();
+
         return response()->json([
-            'message' => 'Cập nhật tài khoản không thành công',
-            'user' => $user,
-        ], 404);  
-    } 
+            'message' => 'Mật khẩu đã được cập nhật thành công'
+        ]);
     }
 }
